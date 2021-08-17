d0303cf85b10901be5d86f1bcfe8cd23f7ff2712
EternalStar <jiangyiqi99@hotmail.com>
2021-08-17 21:58:15 +0800

    Complete LookupWhiteLine

--- Case1.c
+++ Case1.c
@@ -5,6 +5,9 @@
 void PassWhiteLine();
 int LookupWhiteLine();//return 1==Success; 0==Failed
 
+#define Increase 1
+#define Decrease 2
+
 const int eadcTreshold = 180;
 
 void PassWhiteLine() {
@@ -26,14 +29,38 @@ int LookupWhiteLine() {
 	int eadc3 = 0;
 	int eadc3p = 0;
 	int status;
+	int flag = 0;
+	int ReturnValue = 0;
 	for (int tries = 0; tries < 1000; tries++) {
 		if (eadc3 == 0) {
+			//Mark the Current Value
 			eadc3 = geteadc(3);
 			//Here should have A TRY that if Value=100 Could WORK!
 			motor(1, 100);
 		}
+		//Judge Increase or Decrease
 		else if (eadc3 != 0 && eadc3p == 0) {
-
+			eadc3p = eadc3;
+			eadc3 = geteadc(3);
+			if (eadc3 > eadc3p) flag = Increase;
+			if (eadc3 < eadc3p) flag = Decrease;
+			if (eadc3 = eadc3p) eadc3p = 0;
+		}
+		else if (flag==Increase) {
+			//Here should have A TRY that if Value=100 Could WORK!
+			motor(1, -100);
+			eadc3p = eadc3;
+			eadc3 = geteadc(3);
+			flag = Decrease;
+		}
+		else if (flag == Decrease) {
+			eadc3p = eadc3;
+			eadc3 = geteadc(3);
+			if (eadc3p < eadc3) {
+				ReturnValue = 1;
+				break;
+			}
 		}
 	}
+	return ReturnValue;
 }
