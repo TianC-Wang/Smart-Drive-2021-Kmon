dd0834c3ca7e38e7a0a246736ea3e6748251e376
EternalStar <jiangyiqi99@hotmail.com>
2021-08-18 15:42:17 +0800

    Fixed LookupWhiteLineWithSensor()

--- Case1.c
+++ Case1.c
@@ -5,29 +5,58 @@
 #define Increase 1
 #define Decrease 2
 
-//For the use of adcTreshold
-const int adcTreshold = 200;
+//For the use of eadcTreshold1
+const int eadcTreshold1 = 400;
 
 int eadcSensor[6];
 
 void UpdateeadcSensor(void) {
 	for (int a = 0; a < 6; a++) eadcSensor[a] = geteadc(a + 1);
-	printf("%d %d %d %d %d %d", eadcSensor[0], eadcSensor[1], eadcSensor[2], eadcSensor[3], eadcSensor[4],
-		eadcSensor[5]);
+	//printf("%d %d %d %d %d %d\n", eadcSensor[0], eadcSensor[1], eadcSensor[2], eadcSensor[3], eadcSensor[4],eadcSensor[5]);
+}
+
+
+int ToBlackRoad(int TriesLimited) {
+	for (int a = 0; a < TriesLimited; a++) {
+		cls();
+		UpdateeadcSensor();
+		int count = 0;
+		if (eadcSensor[1] > eadcTreshold1) count++;
+		if (eadcSensor[2] > eadcTreshold1) count++;
+		if (eadcSensor[3] > eadcTreshold1) count++;
+		if (eadcSensor[4] > eadcTreshold1) count++;
+		//Can edit the Sensor nunmber of detected
+		if (count >= 2) {
+			return 1;
+		}
+		//}
+		//cls();
+		//UpdateeadcSensor();
+		//int count = 0;
+		//if (eadcSensor[0] > 190) count++;
+		//if (eadcSensor[5] > 235) count++;
+		////Can edit the Sensor nunmber of detected
+		//if (count >= 1) {
+		//	return 1;
+		//}
+	}
+	return 0;
 }
 
 //return 1==Success; 0==Failed
 int PassWhiteLine(int TriesLimited) {
+	ToBlackRoad(TriesLimited);
 	for (int a = 0; a < TriesLimited; a++) {
+		cls();
 		UpdateeadcSensor();
 		int count = 0;
-		if (eadcSensor[1] <= adcTreshold) count++;
-		if (eadcSensor[2] <= adcTreshold) count++;
-		if (eadcSensor[3] <= adcTreshold) count++;
-		if (eadcSensor[4] <= adcTreshold) count++;
+		if (eadcSensor[1] <= eadcTreshold1) count++;
+		if (eadcSensor[2] <= eadcTreshold1) count++;
+		if (eadcSensor[3] <= eadcTreshold1) count++;
+		if (eadcSensor[4] <= eadcTreshold1) count++;
 		//Can edit the Sensor nunmber of detected
 		if (count >= 1) {
-			return 1;
+			return 2;
 		}
 	}
 	return 0;
@@ -79,63 +108,94 @@ int LookupWhiteLineWithDistance(void) {
 
 
 // 他也曾鲜衣怒马，望尽长安花
-int LookupWhiteLineWithSenior(void) {
-	motor(1, 0); motor(2, 0);
+//int LookupWhiteLineWithSenior(void) {
+//	motor(1, 0); motor(2, 0);
+//	UpdateeadcSensor();
+//	if (eadcSensor[0] > eadcTreshold1 && eadcSensor[1] > eadcTreshold1 && eadcSensor[2] > eadcTreshold1 &&
+//		eadcSensor[3] > eadcTreshold1 && eadcSensor[4] > eadcTreshold1 && eadcSensor[5] > eadcTreshold1) {
+//		return 0;
+//	}
+//LookupStarted:
+//	if (eadcSensor[2] <= eadcTreshold1) {
+//		if (eadcSensor[4] > eadcTreshold1) {
+//			motor(2, 180);
+//			//Tries should have A TEST! (Value of a)
+//			for (int a = 0; a < 10000; a++) {
+//				if (geteadc(5) <= eadcTreshold1) {
+//					motor(2, 0);
+//					return 1;
+//				}
+//			}
+//			motor(2, -180);
+//			for (int a = 0; a < 200; a++) {
+//				if (geteadc(5) <= eadcTreshold1) {
+//					motor(2, 0);
+//					return 1;
+//				}
+//			}
+//		}
+//	}
+//	else if (eadcSensor[4] <= eadcTreshold1) {
+//		if (eadcSensor[1] > eadcTreshold1) {
+//			motor(1, 200);
+//			//Tries should have A TEST! (Value of a)
+//			for (int a = 0; a < 50; a++) {
+//				if (geteadc(2) <= eadcTreshold1) {
+//					motor(1, 0);
+//					return 1;
+//				}
+//			}
+//			motor(2, -200);
+//			for (int a = 0; a < 100; a++) {
+//				if (geteadc(2) <= eadcTreshold1) {
+//					motor(1, 0);
+//					return 1;
+//				}
+//			}
+//		}
+//	}
+//	else {
+//		go(100, 100);
+//		for (int a = 0; a < 50; a++) {
+//			UpdateeadcSensor();
+//			if (eadcSensor[1] <= eadcTreshold1 || eadcSensor[4] <= eadcTreshold1) {
+//				goto LookupStarted;
+//			}
+//		}
+//	}
+//	motor(1, 0); motor(2, 0);
+//	return 0;
+//}
+
+
+//那年我们错过的爱情，是我们如今才会不去的曾经
+int LookupWhiteLineWithSensor(void) {
 	UpdateeadcSensor();
-	if (eadcSensor[0] > adcTreshold && eadcSensor[1] > adcTreshold && eadcSensor[2] > adcTreshold &&
-		eadcSensor[3] > adcTreshold && eadcSensor[4] > adcTreshold && eadcSensor[5] > adcTreshold) {
+	if (eadcSensor[0] > eadcTreshold1 && eadcSensor[1] > eadcTreshold1 && eadcSensor[2] > eadcTreshold1 &&
+		eadcSensor[3] > eadcTreshold1 && eadcSensor[4] > eadcTreshold1 && eadcSensor[5] > eadcTreshold1) {
 		return 0;
 	}
-LookupStarted:
-	if (eadcSensor[1] <= adcTreshold) {
-		if (eadcSensor[4] > adcTreshold) {
-			motor(2, 100);
-			//Tries should have A TEST! (Value of a)
-			for (int a = 0; a < 50; a++) {
-				if (geteadc(5) <= adcTreshold) {
-					motor(2, 0);
-					return 1;
-				}
-			}
-			motor(2, -100);
-			for (int a = 0; a < 100; a++) {
-				if (geteadc(5) <= adcTreshold) {
-					motor(2, 0);
-					return 1;
-				}
-			}
+	motor(1, 220); motor(2, 220);
+	char flagL = 0;
+	char flagR = 0;
+	while (1) {
+		UpdateeadcSensor();
+		if (eadcSensor[0] <= 190) {
+			flagL = 1;
+			motor(1, -180);
 		}
-	}
-	else if (eadcSensor[4] <= adcTreshold) {
-		if (eadcSensor[1] > adcTreshold) {
-			motor(1, 100);
-			//Tries should have A TEST! (Value of a)
-			for (int a = 0; a < 50; a++) {
-				if (geteadc(2) <= adcTreshold) {
-					motor(1, 0);
-					return 1;
-				}
-			}
-			motor(2, -100);
-			for (int a = 0; a < 100; a++) {
-				if (geteadc(2) <= adcTreshold) {
-					motor(1, 0);
-					return 1;
-				}
-			}
+		if (eadcSensor[5] <= 235) {
+			flagR = 1;
+			motor(2, -180);
 		}
-	}
-	else {
-		go(100, 100);
-		for (int a = 0; a < 50; a++) {
+		if (flagL == 1 && flagR == 1) {
+			wait(0.1);
+			motor(1, 0); motor(2, 0);
 			UpdateeadcSensor();
-			if (eadcSensor[1] <= adcTreshold || eadcSensor[4] <= adcTreshold) {
-				goto LookupStarted;
-			}
+			printf("%d %d", eadcSensor[0], eadcSensor[5]);
+			return 1;
 		}
 	}
-	motor(1, 0); motor(2, 0);
-	return 0;
 }
 
 void Display(void) {
@@ -163,13 +223,16 @@ void RoadPlay(void) {
 int main(void) {
 	printf("成功！\n");
 
-	Display();
+	//Display();
 
 	motor(1, 200); motor(2, 200);
 	int debug = PassWhiteLine(10000);
 	if (debug == 1)printf("已过白线\n");
 	if (debug == 0)printf("滚！");
 	motor(1, 0); motor(2, 0);
-
+	debug = LookupWhiteLineWithSensor();
+	if (debug == 1)printf("已校准\n");
+	if (debug == 0)printf("滚！");
 	return 0;
 }
+
